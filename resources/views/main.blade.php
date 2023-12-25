1d9e2734ecff8223c0014d21fb9d2201c715ba0b
4eshir <g.kalashnik@mail.ru>
2023-12-25 17:43:10 +0400

    25-12-2023 add fix

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/RegistrationForm.css">
-    <link rel="stylesheet" href="/css/index.css">
-    <link rel="stylesheet" href="/css/welcome.css">
+    <link rel="stylesheet" href="./css/RegistrationForm.css">
+    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="./css/welcome.css">
     <title>Главная</title>
 </head>
 <body>
