d57b9c69ceeb58e535e9c4f5fa3d15c5d239ede7
4eshir <g.kalashnik@mail.ru>
2023-12-25 17:22:46 +0400

    25-12-2023 add fix

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -232,6 +232,7 @@ class SiteController extends Controller
         }*/
 
         $schools = json_decode(Http::get(getenv('STUDENT_URL')."/api/get-schools/1")->body());
+        dd(getenv('STUDENT_URL')."/api/get-schools/1", $schools);
         $municipalities = json_decode(Http::get(getenv('STUDENT_URL')."/api/get-municipalities/1")->body());
 
 
